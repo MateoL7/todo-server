d31c2db7623e1a8f231be0a6ea4d5a2f76d11ea7
Loaiza <Mateo.Loaiza@btgpactual.com>
2023-03-23 17:39:28 +0100

    Last commit, changed branch name

--- daos/users.dao.js
+++ daos/users.dao.js
@@ -1,35 +1,46 @@
-const DBSOURCE = "./todos.sqlite";
+const DBSOURCE = './todos.sqlite';
 
-const betterSqlite3 = require("better-sqlite3");
+const betterSqlite3 = require('better-sqlite3');
 const db = betterSqlite3(DBSOURCE);
 
 function all() {
-  const stm = db.prepare("SELECT * FROM users");
+  const stm = db.prepare('SELECT * FROM users');
   const rows = stm.all();
 
   return rows;
 }
 
 function item(id) {
-  const stm = db.prepare("SELECT * FROM users WHERE id = ?");
+  const stm = db.prepare('SELECT * FROM users WHERE id = ?');
   const rows = stm.get(id);
   return rows;
 }
 
+function getByUsername(username) {
+  const stm = db.prepare('SELECT * FROM users WHERE username = ?');
+  const userFound = stm.get(username);
+  return userFound;
+}
+
 function insert(data) {
   //
+  const stm = db.prepare(
+    'INSERT INTO users (username, password, name, email) VALUES (?,?,?,?)'
+  );
+  const result = stm.run(data.username, data.password, data.name, data.email);
+  return result;
 }
 
 function update(id, done) {
   // done es un boolean y en base de datos es un integer
   const intDone = done ? 1 : 0;
-  const stm = db.prepare("UPDATE todos SET done = ? WHERE id = ?");
+  const stm = db.prepare('UPDATE todos SET done = ? WHERE id = ?');
   const rows = stm.run(intDone, id);
   return rows;
 }
 
 function remove(id) {
-  const stm = db.prepare("DELETE FROM todos WHERE id = ?");
+  const stm = db.prepare('DELETE FROM todos WHERE id = ?');
   const rows = stm.run(id);
   return rows;
 }
@@ -40,4 +51,5 @@ module.exports = {
   item,
   update,
   remove,
+  getByUsername,
 };
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 const cors = require('cors');
@@ -8,12 +9,63 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+const bcrypt = require('bcrypt');
+
+// registrar usuario
+app.post('/register', (req, res, next) => {
+  const { login, password, nombre, email } = req.body;
+
+  const saltRounds = 10;
+  const myPassword = password;
+
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(myPassword, salt, function (err, hash) {
+      //Store hash in your password DB.
+      const userDAO = require('./daos/users.dao');
+      userDAO.insert({
+        username: login,
+        password: hash,
+        name: nombre,
+        email: email,
+      });
+    });
+  });
+});
+
+app.post('/login', (req, res, next) => {
+  const { login, password } = req.body;
+
+  const userDAO = require('./daos/users.dao');
+  const userFound = userDAO.getByUsername(login);
+
+  // Login incorrecto
+  if (!userFound) return next('Login incorrecto');
+
+  if (bcrypt.compareSync(password, userFound.password)) {
+    // Login correcto
+
+    // Generar token para mandar al cliente
+    var jwt = require('jsonwebtoken');
+    var token = jwt.sign(
+      { id: userFound.id, name: userFound.name },
+      process.env.PASSWORD_JWT
+    );
+    res.status(200).json({ token: token });
+  } else {
+    // Login incorrecto
+    return next('Login incorrecto');
+  }
+});
 const usersRouter = require('./routers/users.router.js');
 
 app.use('/users', usersRouter);
 
 app.all('*', (req, res) => {
   res.status(404).json({ error: 'esta no es la página que estabas buscando' });
+});
+
+app.use((error, req, res, next) => {
+  res.status(401).json({ error: 'Login incorrecto' });
 });
 
 app.listen(port, () => {
